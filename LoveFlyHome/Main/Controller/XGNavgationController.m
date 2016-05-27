ba9d86f2e58b224ac58e8b13f1da25c3247b9e2b
Lefeng <m15213202723@163.com>
2016-05-27 11:49:28 +0800

    这是我测试提交的

--- LoveFlyHome/Main/Controller/XGNavgationController.m
+++ LoveFlyHome/Main/Controller/XGNavgationController.m
@@ -5,7 +5,7 @@
 //  Created by Lefeng on 16/5/26.
 //  Copyright © 2016年 Lefeng. All rights reserved.
 //
-
+//  我新写的东西要提交了  这里是我新写的啊
 #import "XGNavgationController.h"
 
 @interface XGNavgationController ()
